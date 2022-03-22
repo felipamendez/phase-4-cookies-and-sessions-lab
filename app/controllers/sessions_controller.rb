2e9606bad9a00cb1ab057560132baf9400655f23
Felipa Mendez <felipalouise@gmail.com>
2022-03-22 11:55:30 -0500

    commit tree baaa271efa734a3c59113842748d94fb42dd17be

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+    # def show
+    #     article = Article.find_by(id: params[:id])
+    #     session[:page_views] ||= 0
+    #     session[:page_views] += 1
+    #     render json: article
+    # end
+
+end
